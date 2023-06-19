06574b2735f7529088ae8251ad4c962f3aa17e89
LuisPatricioGit <luispatricio.info@gmail.com>
2023-06-19 16:47:44 +0100

    Add project files.

---
+++ CompanhiaAguas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompanhiaAguas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
