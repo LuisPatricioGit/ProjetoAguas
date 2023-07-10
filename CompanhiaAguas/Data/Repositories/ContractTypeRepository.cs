0ed943f0972bdfaaef1fd00a5914a25b76a241d6
LuisPatricioGit <luispatricio.info@gmail.com>
2023-07-10 15:52:13 +0100

    Generic Repository

---
+++ CompanhiaAguas/Data/Repositories/ContractTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using CompanhiaAguas.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanhiaAguas.Data.Repositories
+{
+    public class ContractTypeRepository : GenericRepository<ContractType>, IContractTypeRepository
+    {
+        private readonly DataContext _context;
+        public ContractTypeRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
